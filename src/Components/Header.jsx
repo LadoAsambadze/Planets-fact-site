5d0603decbb7533c5c27e544e842e8bccac591db
Lado Asambadze <lado.asambadze1@gmail.com>
2023-04-26 00:31:59 +0400

    make changes in header and creat new page menu on the place of some items from header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,7 @@
 import { Link } from "react-router-dom";
 import buttonMobile from "../../public/assets/icon-hamburger.svg";
-import arrow from "../../public/assets/icon-chevron.svg";
-import { useState } from "react";
+
 export default function Header() {
-  const [active, setActive] = useState(false);
-
-  function show() {
-    setActive(!active);
-  }
-
   return (
     <>
       <div className="flex justify-between md:justify-center md:flex-col items-center px-[24px] md:px-[51px] pb-[17px]">
@@ -65,9 +58,8 @@ export default function Header() {
             neptune
           </Link>
         </div>
-        <Link to="/planets/menu">
+        <Link to="/menu">
           <img
-            onClick={show}
             src={buttonMobile}
             className="button-for-list cursor-pointer md:hidden "
             alt="button"
@@ -75,127 +67,6 @@ export default function Header() {
         </Link>
       </div>
       <div className="h-px opacity-20 mix-blend-normal w-full bg-white"></div>
-      {active ? (
-        <div className="md:hidden ">
-          <div className="pt-[24px] px-[24px]  w-full">
-            <div className="flex items-center flex-row justify-between w-full mt-[20px]">
-              <div className="flex items-center justify-center">
-                <div className="bg-[#DEF4FC] w-[20px] h-[20px] rounded-full"></div>
-                <Link
-                  onClick={show}
-                  to="/planets/mercury"
-                  className="text-white font-[spartan] ml-[25px] font-bold tracking-[1.3px] uppercase text-[15px] leading-[25px]"
-                >
-                  mercury
-                </Link>
-              </div>
-              <img src={arrow} alt="arrow button" />
-            </div>
-            <div className="h-px opacity-10 mix-blend-normal w-full bg-white mt-[23px]"></div>
-            <div className="flex items-center flex-row justify-between w-full mt-[20px]">
-              <div className="flex items-center justify-center">
-                <div className="bg-[#F7CC7F] w-[20px] h-[20px] rounded-full"></div>
-                <Link
-                  to="/planets/venus"
-                  className="text-white font-[spartan] ml-[25px] font-bold tracking-[1.3px] uppercase text-[15px] leading-[25px]"
-                >
-                  venus
-                </Link>
-              </div>
-              <img src={arrow} alt="arrow button" />
-            </div>
-            <div className="h-px opacity-10 mix-blend-normal w-full bg-white mt-[23px]"></div>
-
-            <div className="flex items-center flex-row justify-between w-full mt-[20px]">
-              <div className="flex items-center justify-center">
-                <div className="bg-[#545BFE] w-[20px] h-[20px] rounded-full"></div>
-                <Link
-                  to="/planets/earth"
-                  className="text-white font-[spartan] ml-[25px] font-bold tracking-[1.3px] uppercase text-[15px] leading-[25px]"
-                >
-                  earth
-                </Link>
-              </div>
-              <img src={arrow} alt="arrow button" />
-            </div>
-            <div className="h-px opacity-10 mix-blend-normal w-full bg-white mt-[23px]"></div>
-
-            <div className="flex items-center flex-row justify-between w-full mt-[20px]">
-              <div className="flex items-center justify-center">
-                <div className="bg-[#FF6A45] w-[20px] h-[20px] rounded-full"></div>
-                <Link
-                  to="/planets/mars"
-                  className="text-white font-[spartan] ml-[25px] font-bold tracking-[1.3px] uppercase text-[15px] leading-[25px]"
-                >
-                  mars
-                </Link>
-              </div>
-              <img src={arrow} alt="arrow button" />
-            </div>
-            <div className="h-px opacity-10 mix-blend-normal w-full bg-white mt-[23px]"></div>
-
-            <div className="flex items-center flex-row justify-between w-full mt-[20px]">
-              <div className="flex items-center justify-center">
-                <div className="bg-[#ECAD7A] w-[20px] h-[20px] rounded-full"></div>
-                <Link
-                  to="/planets/jupiter"
-                  className="text-white font-[spartan] ml-[25px] font-bold tracking-[1.3px] uppercase text-[15px] leading-[25px]"
-                >
-                  jupiter
-                </Link>
-              </div>
-              <img src={arrow} alt="arrow button" />
-            </div>
-            <div className="h-px opacity-10 mix-blend-normal w-full bg-white mt-[23px]"></div>
-
-            <div className="flex items-center flex-row justify-between w-full mt-[20px]">
-              <div className="flex items-center justify-center">
-                <div className="bg-[#FCCB6B] w-[20px] h-[20px] rounded-full"></div>
-                <Link
-                  to="/planets/saturn"
-                  className="text-white font-[spartan] ml-[25px] font-bold tracking-[1.3px] uppercase text-[15px] leading-[25px]"
-                >
-                  saturn
-                </Link>
-              </div>
-              <img src={arrow} alt="arrow button" />
-            </div>
-            <div className="h-px opacity-10 mix-blend-normal w-full bg-white mt-[23px]"></div>
-
-            <div className="flex items-center flex-row justify-between w-full mt-[20px]">
-              <div className="flex items-center justify-center">
-                <div className="bg-[#FCCB6B] w-[20px] h-[20px] rounded-full"></div>
-                <Link
-                  to="/planets/uranus"
-                  className="text-white font-[spartan] ml-[25px] font-bold tracking-[1.3px] uppercase text-[15px] leading-[25px]"
-                >
-                  uranus
-                </Link>
-              </div>
-              <img src={arrow} alt="arrow button" />
-            </div>
-            <div className="h-px opacity-10 mix-blend-normal w-full bg-white mt-[23px]"></div>
-
-            <div className="flex items-center flex-row justify-between w-full mt-[20px]">
-              <div
-                className="flex items-cent
-            
-            er justify-center"
-              >
-                <div className="bg-[#497EFA] w-[20px] h-[20px] rounded-full"></div>
-                <Link
-                  to="/planets/neptune"
-                  className="text-white font-[spartan] ml-[25px] font-bold tracking-[1.3px] uppercase text-[15px] leading-[25px]"
-                >
-                  neptune
-                </Link>
-              </div>
-              <img src={arrow} alt="arrow button" />
-            </div>
-            <div className="h-px opacity-10 mix-blend-normal w-full bg-white mt-[23px]"></div>
-          </div>
-        </div>
-      ) : null}
     </>
   );
 }
