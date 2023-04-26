bf3856e6190b98d24112e0de236d3dcf07256723
Lado Asambadze <lado.asambadze1@gmail.com>
2023-04-26 16:38:46 +0400

    create desktop resp.design mark up and finish write logic

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,59 +4,80 @@ import buttonMobile from "../../public/assets/icon-hamburger.svg";
 export default function Header() {
   return (
     <>
-      <div className="flex justify-between md:justify-center md:flex-col items-center px-[24px] md:px-[51px] pb-[17px]">
-        <h1 className="text-[#FFFFFF] font-[antonio] text-[28px] leading-9 font-[400] tracking-[-1px] ">
+      <div
+        className="flex justify-between md:justify-center md:flex-col items-center px-[24px] md:px-[51px] pb-[17px] md:pb-[22px] xl:pt-0
+      xl:flex-row  xl:pr-[41px] xl:pb-[27px] xl:pl-[32px] xl:items-center  xl:justify-evenly md:pt-[32px]"
+      >
+        <h1 className="text-[#FFFFFF] font-[antonio, sans-serif] text-[28px] leading-9 font-[400] tracking-[-1px] w-full md:text-center xl:text-left xl:mt-[15px]">
           THE PLANETS
         </h1>
-        <div className="w-full flex justify-evenly items-center dr-row mt-[39px] hidden md:inline-flex">
-          <Link
-            to="/planets/mercury"
-            className="text-[11px] text-white font-[spartan] font-bold tracking-[1px] uppercase leading-[25px]"
-          >
-            mercury
-          </Link>
-          <Link
-            to="/planets/venus"
-            className="text-[11px] text-white font-[spartan] font-bold tracking-[1px] uppercase leading-[25px]"
-          >
-            venus
-          </Link>
-          <Link
-            to="/planets/earth"
-            className="text-[11px] text-white font-[spartan] font-bold tracking-[1px] uppercase leading-[25px]"
-          >
-            earth
-          </Link>
-          <Link
-            to="/planets/mars"
-            className="text-[11px] text-white font-[spartan] font-bold tracking-[1px] uppercase leading-[25px]"
-          >
-            mars
-          </Link>
-          <Link
-            to="/planets/jupiter"
-            className="text-[11px] text-white font-[spartan] font-bold tracking-[1px] uppercase leading-[25px]"
-          >
-            jupiter
-          </Link>
-          <Link
-            to="/planets/saturn"
-            className="text-[11px] text-white font-[spartan] font-bold tracking-[1px] uppercase leading-[25px]"
-          >
-            saturn
-          </Link>
-          <Link
-            to="/planets/uranus"
-            className="text-[11px] text-white font-[spartan] font-bold tracking-[1px] uppercase leading-[25px]"
-          >
-            uranus
-          </Link>
-          <Link
-            to="/planets/neptune"
-            className="text-[11px] text-white font-[spartan] font-bold tracking-[1px] uppercase leading-[25px]"
-          >
-            neptune
-          </Link>
+        <div className="w-full flex justify-evenly items-center flex-row  hidden md:inline-flex md:mt-[39px]  xl:mt-0 ">
+          <div className="flex flex-col border-t-4 hover:border-[#419EBB]   border-[#070724]">
+            <Link
+              to="/planets/mercury"
+              className="text-[11px] text-white font-[spartan] font-bold tracking-[1px] uppercase leading-[25px]  xl:mt-[22px]
+            "
+            >
+              mercury
+            </Link>
+          </div>
+          <div className="flex flex-col border-t-4 hover:border-[#419EBB]   border-[#070724]">
+            <Link
+              to="/planets/venus"
+              className="text-[11px] text-white font-[spartan] font-bold tracking-[1px] uppercase leading-[25px] xl:mt-[22px]
+            "
+            >
+              venus
+            </Link>
+          </div>
+          <div className="flex flex-col border-t-4 hover:border-[#6D2ED5]   border-[#070724]">
+            <Link
+              to="/planets/earth"
+              className="text-[11px] text-white font-[spartan] font-bold tracking-[1px] uppercase leading-[25px] xl:mt-[22px]"
+            >
+              earth
+            </Link>
+          </div>
+          <div className="flex flex-col border-t-4 hover:border-[#D14C32]   border-[#070724]">
+            <Link
+              to="/planets/mars"
+              className="text-[11px] text-white font-[spartan] font-bold tracking-[1px] uppercase leading-[25px] xl:mt-[22px]"
+            >
+              mars
+            </Link>
+          </div>
+          <div className="flex flex-col border-t-4 hover:border-[#D83A34]   border-[#070724]">
+            <Link
+              to="/planets/jupiter"
+              className="text-[11px] text-white font-[spartan] font-bold tracking-[1px] uppercase leading-[25px] xl:mt-[22px]"
+            >
+              jupiter
+            </Link>
+          </div>
+          <div className="flex flex-col border-t-4 hover:border-[#CD5120]   border-[#070724]">
+            <Link
+              to="/planets/saturn"
+              className="text-[11px] text-white font-[spartan] font-bold tracking-[1px] uppercase leading-[25px] xl:mt-[22px]"
+            >
+              saturn
+            </Link>
+          </div>
+          <div className="flex flex-col border-t-4 hover:border-[#1EC1A2]   border-[#070724]">
+            <Link
+              to="/planets/uranus"
+              className="text-[11px] text-white font-[spartan] font-bold tracking-[1px] uppercase leading-[25px] xl:mt-[22px]"
+            >
+              uranus
+            </Link>
+          </div>
+          <div className="flex flex-col border-t-4 hover:border-[#2D68F0]   border-[#070724]">
+            <Link
+              to="/planets/neptune"
+              className="text-[11px] text-white font-[spartan] font-bold tracking-[1px] uppercase leading-[25px] xl:mt-[22px]"
+            >
+              neptune
+            </Link>
+          </div>
         </div>
         <Link to="/menu">
           <img
--- src/pages/planets.jsx
+++ src/pages/planets.jsx
@@ -23,178 +23,260 @@ export default function Main() {
 
   return (
     <>
-      <div className="px-[24px] py-[20px] w-full flex flex-row justify-evenly items-center md:hidden  ">
-        <p
-          onClick={forOverview}
-          className="text-white font-[spartan] text-[9px] leading-[10px] uppercase tracking-[1.92px] font-bold"
+      <div className="px-[24px] pt-[20px] w-full flex flex-row justify-evenly items-center md:hidden    ">
+        <div
+          className="border-[#070724] border-b-4   cursor-pointer"
+          style={{
+            borderColor: active === "overview" ? planet.color : null,
+          }}
         >
-          overview
-        </p>
-        <p
-          onClick={forStructure}
-          className="text-white font-[spartan] text-[9px] leading-[10px] uppercase tracking-[1.92px] font-bold"
-        >
-          structure
-        </p>
-        <p
-          onClick={forSurface}
-          className="text-white font-[spartan] text-[9px] leading-[10px] uppercase tracking-[1.92px] font-bold"
-        >
-          surface
-        </p>
+          <p
+            onClick={forOverview}
+            className="text-white font-[spartan] text-[9px] leading-[10px] uppercase tracking-[1.92px] font-bold pb-[17px] "
+          >
+            overview
+          </p>
+        </div>
+        <div className="border-[#070724] border-b-4   cursor-pointer"
+         style={{
+          borderColor: active === "structure" ? planet.color : null,
+        }}>
+          <p
+            onClick={forStructure}
+            className="text-white font-[spartan] text-[9px] leading-[10px] uppercase tracking-[1.92px] font-bold pb-[17px]"
+          >
+            structure
+          </p>
+        </div>
+        <div className="border-[#070724] border-b-4  h cursor-pointer"
+         style={{
+          borderColor: active === "surface" ? planet.color : null,
+        }}>
+          <p
+            onClick={forSurface}
+            className="text-white font-[spartan] text-[9px] leading-[10px] uppercase tracking-[1.92px] font-bold pb-[17px] "
+          >
+            surface
+          </p>
+        </div>
       </div>
       <div className="h-[1px] bg-white opacity-20 w-full md:hidden "></div>
 
-      <div className="flex flex-col justify-center items-center px-[24px] ">
-        <img
-          className="mt-[95px] md:mt-[146px] w-[40%] md:w-[15%]"
-          src={
-            active === "overview"
-              ? planet.images.planet
-              : active === "structure"
-              ? planet.images.internal
-              : active === "surface"
-              ? planet.images.planet && planet.images.geology
-              : null
-          }
-        />
-        <div className="flex flex-col mt-[98px] justify-center md:mt-[130px] w-full md:flex-row md:flex-col">
-          <div className="w-full flex justify-center  md:flex-row md:justify-between">
-            <div className="md:w-[50%]">
-              <h1 className="text-[40px] font-[antonio] uppercase text-white text-center font-normal leading-[52px] md:text-[48px] md:leading-[62px] md:text-left">
-                {planet.name}
-              </h1>
-              <p className="text-[#FFFFFF] font-[spartan] text-[11px] leading-[22px] font-normal text-center mt-[16px] md:text-left ">
-                {active === "overview"
-                  ? planet.overview.content
-                  : active === "structure"
-                  ? planet.structure.content
-                  : active === "surface"
-                  ? planet.geology.content
-                  : null}
-              </p>
-              <div className="flex flex-row justify-center mt-[32px] w-full md:justify-start">
-                <p className="ml-[4px] text-white text-[12px] leading-[25px] opacity-50">
-                  Source:
-                </p>
-                <a
-                  href={
-                    active === "overview"
-                      ? planet.overview.source
-                      : active === "structure"
-                      ? planet.structure.source
-                      : active === "surface"
-                      ? planet.geology.source
-                      : null
-                  }
-                  className="ml-[4px] text-white text-[12px] leading-[25px] opacity-50 md:text-left  "
-                >
-                  Wikipedia
-                </a>
-                <img />
-              </div>
+      <div className="flex flex-col justify-center items-center px-[24px] xl:px-[165px] xl:pt-[126px] xl:pb-[56px] xl:flex-row xl:justify-start xl:items-start ">
+        <div className="h-[304px] xl:hidden md:h-[460px] flex justify-center items-center ">
+          <img
+            className=" max-w-[80%] max-h-[80%] xl:hidden  "
+            src={
+              active === "overview"
+                ? planet.images.planet
+                : active === "structure"
+                ? planet.images.internal
+                : active === "surface"
+                ? planet.images.geology
+                : null
+            }
+          />
+        </div>
+        <div className="flex flex-col  justify-center  w-full md:flex-row md:flex-col xl:mt-0  ">
+          <div className="w-full flex justify-center  md:flex-row md:justify-between xl:flex-row ">
+            <div className="w-[100%] h-[400px] hidden xl:flex xl:justify-center xl:items-center">
+              <img
+                className=" max-w-full max-h-full "
+                src={
+                  active === "overview"
+                    ? planet.images.planet
+                    : active === "structure"
+                    ? planet.images.internal
+                    : active === "surface"
+                    ? planet.images.planet && planet.images.geology
+                    : null
+                }
+              />
             </div>
-            <div className="flex flex-col hidden w-[280px] md:block  w-[50%] pl-[70px]">
-              <div
-                onClick={forOverview}
-                className="flex flex-row px-[24px] pt-[9px] pb-[13px]  items-center  w-full border-[1px] border-solid mt-[8px] py-[8px]
-              pl-[20px] pr-[161px]"
-              >
-                <p
-                  className="uppercase opacity-50  text-white tracking-[0.7px] text-[8px] leading-[16px] font-[spartan] mix-blend-normal 
-                  md:text-[9px] mde:leading-[25px] tracking-[1.9px]"
+            <div className="flex flex-row items-end xl:flex-col xl:w-[80%]">
+              <div className="md:w-[50%] xl:w-[75%] ">
+                <h1
+                  className="text-[40px] font-[antonio, sans-serif] uppercase text-white text-center font-normal leading-[52px] md:text-[48px] md:leading-[62px] md:text-left 
+                xl:text-left xl:text-[80px] leading-[104px]"
                 >
-                  01
+                  {planet.name}
+                </h1>
+                <p
+                  className="text-[#FFFFFF] font-[spartan] text-[11px] leading-[22px] font-normal text-center mt-[16px] md:text-left
+                 xl:text-left xl:mt-[23px] xl:text-[14px] xl:leading-[25px]"
+                >
+                  {active === "overview"
+                    ? planet.overview.content
+                    : active === "structure"
+                    ? planet.structure.content
+                    : active === "surface"
+                    ? planet.geology.content
+                    : null}
                 </p>
-                <p className="text-white font-[antonio] font-normal uppercase text-[20px] leading-[26px] pl-[17px] md:text-[9px] md:leading-[25px] tracking-[1.9px] ">
-                  overview
-                </p>
+                <div className="flex flex-row justify-center mt-[32px] w-full md:justify-start  xl:mt-[24px]">
+                  <p className="ml-[4px] text-white text-[12px] leading-[25px] opacity-50 xl:text-[14px] xl:leading-[25px]">
+                    Source:
+                  </p>
+                  <a
+                    href={
+                      active === "overview"
+                        ? planet.overview.source
+                        : active === "structure"
+                        ? planet.structure.source
+                        : active === "surface"
+                        ? planet.geology.source
+                        : null
+                    }
+                    className="ml-[4px] text-white text-[12px] leading-[25px] opacity-50 md:text-left  xl:text-[14px] xl:leading-[25px] "
+                  >
+                    Wikipedia
+                  </a>
+                  <img />
+                </div>
               </div>
-              <div
-                onClick={forStructure}
-                className="flex flex-row px-[24px] pt-[9px] pb-[13px]  items-center  w-full border-[1px] border-solid mt-[8px] py-[8px]
-              pl-[20px] pr-[87px]"
-              >
-                <p
-                  className="uppercase opacity-50  text-white tracking-[0.7px] text-[8px] leading-[16px] font-[spartan] mix-blend-normal 
-                  md:text-[9px] mde:leading-[25px] tracking-[1.9px]"
+              <div className="flex flex-col hidden w-[280px] md:block md:w-[50%]  xl:w-[75%] pl-[70px] xl:pl-0">
+                <div
+                  onClick={forOverview}
+                  className="flex flex-row px-[24px] pt-[9px] pb-[13px]  items-center  w-full border-[1px] border-solid mt-[8px] py-[8px]
+              pl-[20px] pr-[161px]  cursor-pointer  hover:bg-[#D8D8D8] hover:bg-opacity-20 hover:mix-blend-normal "
+                  style={{
+                    backgroundColor:
+                      active === "overview" ? planet.color : null,
+                  }}
                 >
-                  02
-                </p>
-                <p className="text-white font-[antonio] font-normal uppercase text-[20px] leading-[26px] pl-[17px] md:text-[9px] md:leading-[25px] tracking-[1.9px] ">
-                  Internal Structure
-                </p>
-              </div>
-              <div
-                onClick={forSurface}
-                className="flex flex-row px-[24px] pt-[9px] pb-[13px]  items-center  w-full border-[1px] border-solid mt-[8px] py-[8px]
-              pl-[20px] pr-[105px]"
-              >
-                <p
-                  className="uppercase opacity-50  text-white tracking-[0.7px] text-[8px] leading-[16px] font-[spartan] mix-blend-normal 
-                  md:text-[9px] mde:leading-[25px] tracking-[1.9px]"
+                  <p
+                    className="uppercase opacity-50  text-white tracking-[0.7px] text-[8px] leading-[16px] font-[spartan] mix-blend-normal 
+                  md:text-[9px] md:leading-[25px] tracking-[1.9px] xl:text-[12px] xl:leading-[25px] tracking-[2.5px]"
+                  >
+                    01
+                  </p>
+                  <p
+                    className="text-white font-[antonio] font-normal uppercase text-[20px] leading-[26px] pl-[17px] md:text-[9px] md:leading-[25px] tracking-[1.9px]
+                  xl:text-[12px] xl:leading-[25px] tracking-[2.5px] "
+                  >
+                    overview
+                  </p>
+                </div>
+                <div
+                  onClick={forStructure}
+                  className="flex flex-row px-[24px] pt-[9px] pb-[13px]  items-center  w-full border-[1px] border-solid mt-[8px] py-[8px]
+              pl-[20px] pr-[87px]  hover:bg-[#D8D8D8] hover:bg-opacity-20 hover:mix-blend-normal  cursor-pointer"
+                  style={{
+                    backgroundColor:
+                      active === "structure" ? planet.color : null,
+                  }}
                 >
-                  03
-                </p>
-                <p className="text-white font-[antonio] font-normal uppercase text-[20px] leading-[26px] pl-[17px] md:text-[9px] md:leading-[25px] tracking-[1.9px] ">
-                  Surface Geology
-                </p>
+                  <p
+                    className="uppercase opacity-50  text-white tracking-[0.7px] text-[8px] leading-[16px] font-[spartan] mix-blend-normal 
+                  md:text-[9px] mde:leading-[25px] tracking-[1.9px] xl:text-[12px] xl:leading-[25px] tracking-[2.5px]"
+                  >
+                    02
+                  </p>
+                  <p
+                    className="text-white font-[antonio] font-normal uppercase text-[20px] leading-[26px] pl-[17px] md:text-[9px] md:leading-[25px] tracking-[1.9px]
+                  xl:text-[12px] xl:leading-[25px] tracking-[2.5px] "
+                  >
+                    Internal Structure
+                  </p>
+                </div>
+                <div
+                  onClick={forSurface}
+                  className="flex flex-row px-[24px] pt-[9px] pb-[13px]  items-center  w-full border-[1px] border-solid mt-[8px] py-[8px]
+              pl-[20px] pr-[105px] hover:bg-[#D8D8D8] hover:bg-opacity-20 hover:mix-blend-normal  cursor-pointer "
+                  style={{
+                    backgroundColor: active === "surface" ? planet.color : null,
+                  }}
+                >
+                  <p
+                    className="uppercase opacity-50  text-white tracking-[0.7px] text-[8px] leading-[16px] font-[spartan] mix-blend-normal 
+                  md:text-[9px] mde:leading-[25px] tracking-[1.9px] xl:text-[12px] xl:leading-[25px] tracking-[2.5px]"
+                  >
+                    03
+                  </p>
+                  <p
+                    className="text-white font-[antonio] font-normal uppercase text-[20px] leading-[26px] pl-[17px] md:text-[9px] md:leading-[25px] tracking-[1.9px] 
+                  xl:text-[12px] xl:leading-[25px] tracking-[2.5px] "
+                  >
+                    Surface Geology
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="flex flex-col w-full mt-[20px] md:flex-row md:justify-between">
+          <div className="flex flex-col w-full mt-[20px] md:flex-row md:justify-between xl:mt-[87px]">
             <div
               className="flex flex-row px-[24px] pt-[9px] pb-[13px] justify-between items-center  border-[1px] border-solid mt-[8px] 
-            md:flex-col md:pt-[16px] md:pb-[19px] md:pl-[15px] md:justify-start md:pr-[0px] md:items-start  md:mt-[0px] md:w-[24%] "
+            md:flex-col md:pt-[16px] md:pb-[19px] md:pl-[15px] md:justify-start md:pr-[0px] md:items-start  md:mt-[0px] md:w-[24%]
+            xl:pt-[20px] xl:pb-[27px] xl:pl-[23px] "
             >
-              <p className="uppercase opacity-50  text-white tracking-[0.7px] text-[8px] leading-[16px] font-[spartan] mix-blend-normal md:text-left">
+              <p
+                className="uppercase opacity-50  text-white tracking-[0.7px] text-[8px] leading-[16px] font-[spartan] mix-blend-normal md:text-left
+              xl:text-[11px] xl:leading-[25px] xl:tracking-[1px]"
+              >
                 Rotation time
               </p>
               <p
                 className="text-white font-[antonio] font-normal uppercase  tracking-[-0.75px] text-[20px] leading-[26px]
-               md:text-[24px] md:leading-[31px] md:tracking-[0.9px] "
+               md:text-[24px] md:leading-[31px] md:tracking-[0.9px]
+               xl:text-[40px] xl:leading-[52px] xl:tracking-[-1.5px] "
               >
                 58.6 days
               </p>
             </div>
             <div
               className="flex flex-row px-[24px] pt-[9px] pb-[13px] justify-between items-center  border-[1px] border-solid mt-[8px] 
-            md:flex-col md:pt-[16px] md:pb-[19px] md:pl-[15px] md:justify-start md:pr-[0px] md:items-start  md:mt-[0px] ]  md:w-[24%]  "
+            md:flex-col md:pt-[16px] md:pb-[19px] md:pl-[15px] md:justify-start md:pr-[0px] md:items-start  md:mt-[0px] ]  md:w-[24%]
+            xl:pt-[20px] xl:pb-[27px] xl:pl-[23px]  "
             >
-              <p className="uppercase opacity-50  text-white tracking-[0.7px] text-[8px] leading-[16px] font-[spartan] mix-blend-normal md:text-left">
+              <p
+                className="uppercase opacity-50  text-white tracking-[0.7px] text-[8px] leading-[16px] font-[spartan] mix-blend-normal md:text-left
+              xl:text-[11px] xl:leading-[25px] xl:tracking-[1px]"
+              >
                 REVOLUTION TIME
               </p>
               <p
                 className="text-white font-[antonio] font-normal uppercase  tracking-[-0.75px] text-[20px] leading-[26px]
-               md:text-[24px] md:leading-[31px] md:tracking-[0.9px] "
+               md:text-[24px] md:leading-[31px] md:tracking-[0.9px]
+               xl:text-[40px] xl:leading-[52px] xl:tracking-[-1.5px]  "
               >
                 87.97 days
               </p>
             </div>
             <div
               className="flex flex-row px-[24px] pt-[9px] pb-[13px] justify-between items-center  border-[1px] border-solid mt-[8px] 
-            md:flex-col md:pt-[16px] md:pb-[19px] md:pl-[15px] md:justify-start md:pr-[0px] md:items-start ] md:mt-[0px] ] md:w-[24%]  "
+            md:flex-col md:pt-[16px] md:pb-[19px] md:pl-[15px] md:justify-start md:pr-[0px] md:items-start ] md:mt-[0px] ] md:w-[24%]
+            xl:pt-[20px] xl:pb-[27px] xl:pl-[23px]  "
             >
-              <p className="uppercase opacity-50  text-white tracking-[0.7px] text-[8px] leading-[16px] font-[spartan] mix-blend-normal md:text-left">
+              <p
+                className="uppercase opacity-50  text-white tracking-[0.7px] text-[8px] leading-[16px] font-[spartan] mix-blend-normal md:text-left
+                xl:text-[11px] xl:leading-[25px] xl:tracking-[1px]
+              "
+              >
                 radius
               </p>
               <p
                 className="text-white font-[antonio] font-normal uppercase  tracking-[-0.75px] text-[20px] leading-[26px]
-               md:text-[24px] md:leading-[31px] md:tracking-[0.9px] "
+               md:text-[24px] md:leading-[31px] md:tracking-[0.9px] 
+               xl:text-[40px] xl:leading-[52px] xl:tracking-[-1.5px] "
               >
                 2,439.7 km
               </p>
             </div>
             <div
               className="flex flex-row px-[24px] pt-[9px] pb-[13px] justify-between items-center  border-[1px] border-solid mt-[8px] 
-            md:flex-col md:pt-[16px] md:pb-[19px] md:pl-[15px] md:justify-start md:pr-[0px] md:items-start  md:mt-[0px] ] md:w-[24%] "
+            md:flex-col md:pt-[16px] md:pb-[19px] md:pl-[15px] md:justify-start md:pr-[0px] md:items-start  md:mt-[0px] ] md:w-[24%]
+            xl:pt-[20px] xl:pb-[27px] xl:pl-[23px] "
             >
-              <p className="uppercase opacity-50  text-white tracking-[0.7px] text-[8px] leading-[16px] font-[spartan] mix-blend-normal md:text-left">
+              <p
+                className="uppercase opacity-50  text-white tracking-[0.7px] text-[8px] leading-[16px] font-[spartan] mix-blend-normal md:text-left
+              xl:text-[11px] xl:leading-[25px] xl:tracking-[1px]"
+              >
                 AVERAGE TEMP.
               </p>
               <p
                 className="text-white font-[antonio] font-normal uppercase  tracking-[-0.75px] text-[20px] leading-[26px]
-               md:text-[24px] md:leading-[31px] md:tracking-[0.9px] "
+               md:text-[24px] md:leading-[31px] md:tracking-[0.9px]
+               xl:text-[40px] xl:leading-[52px] xl:tracking-[-1.5px]  "
               >
                 430°c
               </p>
